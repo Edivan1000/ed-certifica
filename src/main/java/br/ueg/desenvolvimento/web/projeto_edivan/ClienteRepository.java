0350870987b589dbcfb4fb7449be1ae0b1a04c07
Edivan1000 <edivanprofissional10@gmail.com>
2025-05-31 00:40:09 -0300

    Delete src directory

--- src/main/java/br/ueg/desenvolvimento/web/projeto_edivan/ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package br.ueg.desenvolvimento.web.projeto_edivan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
