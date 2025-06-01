b9aa4e6dbf0749bb8827dc275825c9d69513c077
Edivan1000 <edivanprofissional10@gmail.com>
2025-06-01 03:04:57 -0300

    Delete src directory

--- src/main/java/br/ueg/ed/certifica/projeto_edivan/PedidoRepository.java
+++
@@ -1,6 +0,0 @@
-package br.ueg.ed.certifica.projeto_edivan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
